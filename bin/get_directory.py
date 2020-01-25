a7b502996044f0384815bc5c6fd11a186d0895cf
Natsu20 <natsunat85@gmail.com>
2020-01-25 11:35:08 +0900

    bin upload

---
+++ bin/get_directory.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#get home directory
+
+
+# In[2]:
+
+
+import os
+print(os.environ['HOME'])
+
+
+# In[3]:
+
+
+print(os.path.expanduser("~"))
+
